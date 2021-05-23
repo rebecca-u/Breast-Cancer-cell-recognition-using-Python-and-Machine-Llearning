b363e0ff84f02cd437b9f0fa474493f3247a96c3
Rebecca Uddin <84407796+rebecca-u@users.noreply.github.com>
2021-05-23 19:16:09 +0100

    Delete breast cancer project.py

--- breast cancer project.py
+++
@@ -1,8 +0,0 @@
-# Description: This program detects breast cancer, based on a dataset.
-
-#import libraries
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-import keras as ks
-import matplotlib as 
