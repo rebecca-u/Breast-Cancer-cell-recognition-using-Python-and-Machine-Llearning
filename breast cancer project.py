393dd121ef75ef4d2c15ee25430df4f526aa5649
Rebecca Uddin <84407796+rebecca-u@users.noreply.github.com>
2021-05-23 19:15:37 +0100

    Add files via upload

---
+++ breast cancer project.py
@@ -0,0 +1,8 @@
+# Description: This program detects breast cancer, based on a dataset.
+
+#import libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras as ks
+import matplotlib as 
